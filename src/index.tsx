30d21e236c294aa218c11997ae1111645182abe4
xoyn222 <kostuk186@gmail.com>
2023-06-21 15:43:08 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {deepPurple} from "@mui/material/colors";
+import {CssBaseline} from "@mui/material";
+import AppWithReducer from "./AppWithReducer";
+import AppWithRedux from "./AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "./state/store";
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1e88e5',
+        },
+        secondary: deepPurple,
+        mode: 'light'
+    },
+    typography: {
+        fontSize: 16,
+    },
+})
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
- <App />
+    <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Provider store={store}>
+            <AppWithRedux/>
+        </Provider>
+    </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
